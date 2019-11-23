1759e413c3e146fc48d39558e9a5763682b7dd4a
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-11-23 19:17:17 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+//Facil: max 50, 10 intentos
+//Normal: max 100, 8 intentos
+//Dificil: max 150, 6 intentos
+//Muy Dificil: max 500, 5 intentos
+
 let numeroRandom = Number(Math.floor(Math.random() * 50))
 
 function buscaNumero() {
@@ -6,7 +11,7 @@ function buscaNumero() {
 
     if (userNum === numeroRandom) {
         mensaje.className = ("correcto")
-        mensaje.innerText = "Correcto!"
+        mensaje.innerText = "Correcto! Has ganado!"
     } else if (userNum < numeroRandom) {
         mensaje.className = ("incorrecto")
         mensaje.innerText = "Ingresa un numero mas grande!"
