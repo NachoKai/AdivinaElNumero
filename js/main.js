0282b8433b3ef6c8926ec91469d1dd63475b7951
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-01-14 00:40:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,116 +1,123 @@
-let numeroRandom = Number(Math.floor(Math.random() * 50))
-conteoIntentos()
+const numeroRandom = Number(Math.floor(Math.random() * 50));
+conteoIntentos();
 
 function buscaNumero() {
-    let mensaje = document.getElementById("mensaje")
-    let userNum = Number(document.getElementById("user-num").value)
-    let botonIngresar = document.getElementById("button")
+  const mensaje = document.getElementById("mensaje");
+  const userNum = Number(document.getElementById("user-num").value);
+  const botonIngresar = document.getElementById("button");
 
-    if (userNum === numeroRandom) {
-        mensaje.className = ("correcto")
-        mensaje.value = indicarVictoria()
-        botonIngresar.disabled = true
-    } else if (userNum < numeroRandom) {
-        mensaje.className = ("intermedio")
-        mensaje.innerText = "Ingresa un número mas alto! ☝️"
-        restaIntento()
-    } else if (userNum > numeroRandom) {
-        mensaje.className = ("intermedio")
-        mensaje.innerText = "Ingresa un número mas bajo! 👇"
-        restaIntento()
-    } else {
-        mensaje.innerText = "Error! Intenta nuevamente ✋"
-    }
+  if (userNum === numeroRandom) {
+    mensaje.className = "correcto";
+    mensaje.value = indicarVictoria();
+    botonIngresar.disabled = true;
+    return;
+  }
+  if (userNum < numeroRandom) {
+    mensaje.className = "intermedio";
+    mensaje.innerText = "Ingresa un número mas alto! ☝️";
+    restaIntento();
+    return;
+  }
+  if (userNum > numeroRandom) {
+    mensaje.className = "intermedio";
+    mensaje.innerText = "Ingresa un número mas bajo! 👇";
+    restaIntento();
+    return;
+  }
+  mensaje.innerText = "Error! Intenta nuevamente ✋";
 }
-document.getElementById("button").onclick = function () {
-    buscaNumero()
-}
+document.getElementById("button").onclick = () => {
+  buscaNumero();
+};
 
 function conteoIntentos() {
-    let intentos = document.getElementById("intentos")
-    let facil = document.getElementById("facil")
-    let normal = document.getElementById("normal")
-    let dificil = document.getElementById("dificil")
-    let muyDificil = document.getElementById("muy-dificil")
+  const mensaje = document.getElementById("mensaje");
+  const intentos = document.getElementById("intentos");
+  const facil = document.getElementById("facil");
+  const normal = document.getElementById("normal");
+  const dificil = document.getElementById("dificil");
+  const muyDificil = document.getElementById("muy-dificil");
 
-    if (facil.checked === true) {
-        mensaje.className = ("hidden")
-        intentos.value = 10
-    }
-    if (normal.checked === true) {
-        mensaje.className = ("hidden")
-        intentos.value = 8
-    }
-    if (dificil.checked === true) {
-        mensaje.className = ("hidden")
-        intentos.value = 6
-    }
-    if (muyDificil.checked === true) {
-        mensaje.className = ("hidden")
-        intentos.value = 4
-    }
+  if (facil.checked === true) {
+    mensaje.className = "hidden";
+    intentos.value = 10;
+  }
+  if (normal.checked === true) {
+    mensaje.className = "hidden";
+    intentos.value = 8;
+  }
+  if (dificil.checked === true) {
+    mensaje.className = "hidden";
+    intentos.value = 6;
+  }
+  if (muyDificil.checked === true) {
+    mensaje.className = "hidden";
+    intentos.value = 4;
+  }
 }
 
 function restaIntento() {
-    let intentos = document.getElementById("intentos")
-    let botonIngresar = document.getElementById("button")
+  const mensaje = document.getElementById("mensaje");
+  const intentos = document.getElementById("intentos");
+  const botonIngresar = document.getElementById("button");
 
-    if (intentos.value === "1") {
-        mensaje.className = ("incorrecto")
-        mensaje.value = indicarFracaso()
-        botonIngresar.disabled = true
-        intentos.value--
-        intentosMal()
-    } else {
-        intentos.value--
-        intentosMal()
-    }
+  if (intentos.value === "1") {
+    mensaje.className = "incorrecto";
+    mensaje.value = indicarFracaso();
+    botonIngresar.disabled = true;
+    intentos.value--;
+    intentosMal();
+    return;
+  }
+  intentos.value--;
+  intentosMal();
 }
 
-document.getElementById("button").onclick = function () {
-    buscaNumero()
-}
+document.getElementById("button").onclick = () => {
+  buscaNumero();
+};
 
-document.getElementById("dificultad").onclick = function () {
-    conteoIntentos()
-}
+document.getElementById("dificultad").onclick = () => {
+  conteoIntentos();
+};
 
 function indicarFracaso() {
-    Swal.fire({
-        icon: 'error',
-        title: "Perdiste!",
-        html: `
+  Swal.fire({
+    icon: "error",
+    title: "Perdiste!",
+    html: `
         <p class="h4">Agotaste tus intentos.</p>
         <span><input type="button" value="Reiniciar" class="btn btn-outline-danger"
                     onclick="window.location.reload(false)"></span>`,
-        allowOutsideClick: false,
-        allowEscapeKey: false,
-        showConfirmButton: false,
-    })
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    showConfirmButton: false,
+  });
 }
 
 function indicarVictoria() {
-    Swal.fire({
-        icon: 'success',
-        title: "Correcto!",
-        html: `
+  const intentos = document.getElementById("intentos");
+  Swal.fire({
+    icon: "success",
+    title: "Correcto!",
+    html: `
         <p class="h4">Muy bien hecho. Te sobraron ${intentos.value} intentos.</p>
         <span><input type="button" value="Reiniciar" class="btn btn-outline-danger"
                     onclick="window.location.reload(false)"></span>`,
-        allowOutsideClick: false,
-        allowEscapeKey: false,
-        showConfirmButton: false,
-    })
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    showConfirmButton: false,
+  });
 }
 
 function intentosMal() {
-    let intentos = document.getElementById("intentos")
-    setTimeout(() => {
-        intentos.id = 'intentosMal'
-    }, 100);
-    setTimeout(() => {
-        intentos.id = 'intentos'
-    }, 300);
+  const intentos = document.getElementById("intentos");
+  setTimeout(() => {
+    intentos.id = "intentosMal";
+  }, 100);
+  setTimeout(() => {
+    intentos.id = "intentos";
+  }, 300);
 }
 
 document.getElementById("current-year").innerHTML = new Date().getFullYear();
